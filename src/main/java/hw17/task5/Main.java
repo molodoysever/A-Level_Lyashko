388eaec743a68b3cef0518ed981797b0df7a8d73
maksimlasko <dpnofficial1@gmail.com>
2022-01-29 21:46:40 +0200

    hw17

--- src/main/java/hw17/task5/Main.java
+++ src/main/java/hw17/task5/Main.java
@@ -1,25 +1,13 @@
 package hw17.task5;
 
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
+import static hw17.task5.BoxUtil.box;
+import static hw17.task5.BoxUtil.limit;
+
 public class Main {
-    private static final Random random = new Random();
-    public static List<Box> box() {
-        List<Box> box = new LinkedList<>();
-        for (int i = 0; i < 10; i++) {
-            box.add(new Box(random.nextInt(200), list()));
-        }
-        return box;
-    }
-
-    private static int limit() {
-        return random.nextInt(120);
-    }
-
     public static void main(String[] args) {
         System.out.println(box());
         List<Item> sortedList = box().stream()
@@ -28,13 +16,5 @@ public class Main {
                 .sorted(Comparator.comparing(Item::getCost))
                 .collect(Collectors.toList());
         System.out.println(sortedList);
-    }
-
-    public static List<Item> list() {
-        List<Item> item = new LinkedList<>();
-        for (int i = 0; i < random.nextInt(6); i++) {
-            item.add(new Item("name" + random.nextInt(65), random.nextInt(100)));
-        }
-        return item;
     }
 }
