9d0051f8565816272cc828471abb74b321c6425a
maksimlasko <dpnofficial1@gmail.com>
2022-06-16 00:58:52 +0300

    hw15

---
+++ src/main/java/hw15/task2/Box.java
@@ -0,0 +1,13 @@
+package hw15.task2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+
+public class Box {
+    public int volume;
+}
