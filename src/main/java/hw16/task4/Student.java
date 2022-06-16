a9be0dd26f58b46b9e32e0d8e00a77141d80df45
maksimlasko <dpnofficial1@gmail.com>
2022-06-16 14:39:51 +0300

    hw16

---
+++ src/main/java/hw16/task4/Student.java
@@ -0,0 +1,27 @@
+package hw16.task4;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+
+public class Student {
+    private final String name;
+    private final String group;
+    private int course;
+    private final int mark;
+
+    public Student(String name, String group, int course, int mark ) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.mark = mark;
+    }
+
+    public void addCourse (int course) {
+        this.course += course;
+    }
+}
